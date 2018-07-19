eb80abfbcb5735b2d0f133be567977a96578600b
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-07-19 20:51:17 +0200

    Use a custom theme so we can test whether the code adapts to the theme

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
 const withTypescript = require("@zeit/next-typescript");
 
+const urlLoader = mimetype => ({
+  loader: "url-loader",
+  options: {
+    limit: 8192,
+    fallback: "file-loader",
+    mimetype,
+    name: "[name]-[hash].[ext]",
+    publicPath: "/_next/static/",
+    outputPath: "static/"
+  }
+});
+
 module.exports = withTypescript({
   webpack: config => {
     config.module.rules.push({
       test: /\.md$/,
       use: ["catalog/loader", "raw-loader"]
+    });
+    config.module.rules.push({
+      test: /\.woff2$/,
+      use: [urlLoader("font/woff2")]
     });
 
     return config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,64 @@
 import * as React from "react";
 import { Catalog } from "catalog";
+import { injectGlobal } from "emotion";
+
+injectGlobal`
+/* Light */
+@font-face {
+  font-family: Tuna;
+  font-style: normal;
+  font-weight: 300;
+  src: url(${require('../fonts/Tuna/371F3E_3_0.woff2')}) format('woff2');
+}
+
+/* LightItalic */
+@font-face {
+  font-family: Tuna;
+  font-style: italic;
+  font-weight: 300;
+  src: url(${require('../fonts/Tuna/371F3E_5_0.woff2')}) format('woff2');
+}
+
+
+/* Regular */
+@font-face {
+  font-family: Tuna;
+  font-style: normal;
+  font-weight: 400;
+  src: url(${require("../fonts/Tuna/371F3E_7_0.woff2")}) format('woff2');
+}
+
+/* RegularItalic */
+@font-face {
+  font-family: Tuna;
+  font-style: italic;
+  font-weight: 400;
+  src: url(${require('../fonts/Tuna/371F3E_9_0.woff2')}) format('woff2');
+}
+
+/* Bold */
+@font-face {
+  font-family: Tuna;
+  font-style: normal;
+  font-weight: 700;
+  src: url(${require("../fonts/Tuna/371F3E_0_0.woff2")}) format('woff2');
+}
+
+/* BoldItalic */
+@font-face {
+  font-family: Tuna;
+  font-style: italic;
+  font-weight: 700;
+  src: url(${require('../fonts/Tuna/371F3E_1_0.woff2')}) format('woff2');
+}
+`;
+
+/*
+@font-face {font-family: 'Tuna-MediumItalic';src: url('webfonts/371F3E_8_0.eot');src: url('webfonts/371F3E_8_0.eot?#iefix') format('embedded-opentype'),url('webfonts/371F3E_8_0.woff2') format('woff2'),url('webfonts/371F3E_8_0.woff') format('woff'),url('webfonts/371F3E_8_0.ttf') format('truetype');}
+@font-face {font-family: 'Tuna-Heavy';src: url('webfonts/371F3E_2_0.eot');src: url('webfonts/371F3E_2_0.eot?#iefix') format('embedded-opentype'),url('webfonts/371F3E_2_0.woff2') format('woff2'),url('webfonts/371F3E_2_0.woff') format('woff'),url('webfonts/371F3E_2_0.ttf') format('truetype');}
+@font-face {font-family: 'Tuna-HeavyItalic';src: url('webfonts/371F3E_4_0.eot');src: url('webfonts/371F3E_4_0.eot?#iefix') format('embedded-opentype'),url('webfonts/371F3E_4_0.woff2') format('woff2'),url('webfonts/371F3E_4_0.woff') format('woff'),url('webfonts/371F3E_4_0.ttf') format('truetype');}
+@font-face {font-family: 'Tuna-Medium';src: url('webfonts/371F3E_6_0.eot');src: url('webfonts/371F3E_6_0.eot?#iefix') format('embedded-opentype'),url('webfonts/371F3E_6_0.woff2') format('woff2'),url('webfonts/371F3E_6_0.woff') format('woff'),url('webfonts/371F3E_6_0.ttf') format('truetype');}
+*/
 
 interface State {
   isMounted: boolean;
@@ -25,6 +84,34 @@ declare const require: (moduleName: string) => any;
 
 const config = {
   title: "Valde",
+
+  theme: {
+    brandColor: "#460028",
+
+    pageHeadingBackground: "#460028",
+
+    sidebarColor: "#FFFFFF",
+    sidebarColorActive: "#0d68cc",
+    sidebarColorText: "#333333",
+    sidebarColorTextActive: "#0d68cc",
+    sidebarColorLine: "#46002922",
+    sidebarColorHeading: "#460028",
+
+    codeColor: "#00205B",
+    linkColor: "#FF5578",
+    textMedium: "#00205B",
+
+    bgDark: "#333333",
+    bgLight: "#EFEFEF",
+
+    fontFamily: "Tuna, serif",
+    fontHeading: "Tuna, serif",
+    fontMono: "'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace",
+
+    baseFontSize: 18,
+    msRatio: 1.25
+  },
+
   pages: [
     {
       title: "Welcome",
